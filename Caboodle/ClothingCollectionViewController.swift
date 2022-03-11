27849633616d8d104180395a3332f4e7ee7472a6
Vee Nguyen <veeyn@bu.edu>
2022-03-10 16:24:26 -0800

    Removing error from gallery

--- Caboodle/ClothingCollectionViewController.swift
+++ Caboodle/ClothingCollectionViewController.swift
@@ -69,6 +69,9 @@ class ClothingCollectionViewController: UICollectionViewController {
     
         return cell
     }
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            performSegue(withIdentifier: "showDetail", sender: nil)
+        }
 
     // MARK: UICollectionViewDelegate
 
